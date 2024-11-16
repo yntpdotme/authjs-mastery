94d312f73d47456ab7f2489dc8de109a7108e114
Akash Kadlag <akashkadlag14@gmail.com>
2024-11-16 15:59:41 +0530

    feat: build login form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,6 +1,36 @@
+'use client';
+
 import {CardWrapper} from '@/components/auth/card-wrapper';
+import {FormError} from '@/components/form-error';
+import {FormSuccess} from '@/components/form-sucess';
+import {Button} from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {LoginSchema} from '@/schemas';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
 
 export const LoginForm = () => {
+	const form = useForm<z.infer<typeof LoginSchema>>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+		console.log(values);
+	};
+
 	return (
 		<CardWrapper
 			headerLabel="Welcome back"
@@ -8,7 +38,50 @@ export const LoginForm = () => {
 			backButtonHref="/auth/register"
 			showSocial
 		>
-			Login Form
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="email"
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="yntp@example.com"
+											type="email"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({field}) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input {...field} placeholder="******" type="password" />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+
+					<FormError message="" />
+
+					<FormSuccess message="" />
+
+					<Button type="submit" className="w-full">
+						Login
+					</Button>
+				</form>
+			</Form>
 		</CardWrapper>
 	);
 };
---
+++ src/components/form-sucess.tsx
@@ -0,0 +1,16 @@
+import {ExclamationTriangleIcon} from '@radix-ui/react-icons';
+
+type FormSuccessProps = {
+	message?: string;
+};
+
+export const FormSuccess = ({message}: FormSuccessProps) => {
+	if (!message) return null;
+
+	return (
+		<div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
+			<ExclamationTriangleIcon className="size-4" />
+			<p>{message}</p>
+		</div>
+	);
+};
