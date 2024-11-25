66f9611e2b3f9cace691641ae28a0e69fd0b8973
Akash Kadlag <akashkadlag14@gmail.com>
2024-11-25 15:27:47 +0530

    fix: resolve client side session issue

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,7 +1,7 @@
 import {useSession} from 'next-auth/react';
 
 export const useCurrentUser = () => {
-	const session = useSession();
+	const session = useSession({required: true});
 
 	return session.data?.user;
 };
