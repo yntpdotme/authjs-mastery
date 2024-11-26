e829e328799107be1bb268c5f00cac68bff9a68b
Akash Kadlag <akashkadlag14@gmail.com>
2024-11-26 16:36:17 +0530

    style: enhance layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,7 +6,7 @@ type ProtectedLayoutProps = {
 
 const ProtectedLayout = ({children}: ProtectedLayoutProps) => {
 	return (
-		<div className="min-h-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to to-blue-800">
+		<div className="min-h-full flex flex-col gap-y-10 items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to to-blue-800">
 			<Navbar />
 			{children}
 		</div>
